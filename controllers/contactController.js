873321f6d2b38515eb30a60c491c8d599b099b92
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2024-02-01 10:03:52 -0300

    <add>CRUD Operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require('express-async-handler');
+const Contact = require('../models/contactModel');
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get all contacts' });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 //@desc Create all contacts
@@ -18,29 +20,45 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('All fields are mandatory');
   }
-
-  res.status(200).json({ message: 'Create contact', name, email, phone });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(200).json(contact);
 });
 
 //@desc Get individual contact
 //@route GET /api/contacts/:id
 //@access public
 const getIndividualContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error('Contact not found.');
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Update a single contact
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+  const editContact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.status(200).json(editContact);
 });
 
 //@desc Delete a single contact
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id)
+  if (!contact) {
+    res.status(404);
+    throw new Error('Contact not found.');
+  }
+  await Contact.deleteOne();
+  res.status(200).json(contact);
 });
 
 module.exports = {
